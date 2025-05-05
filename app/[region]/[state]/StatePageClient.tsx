71f86046df7caed76e0c9fd54226847b52aded31
BK Jinesh <bkjinesh@BKs-MacBook-Pro.local>
2025-05-05 18:42:12 +0530

    rename footer

--- app/[region]/[state]/StatePageClient.tsx
+++ app/[region]/[state]/StatePageClient.tsx
@@ -176,7 +176,7 @@ export default function StatePageClient({
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
           </svg>
-          Back to {stateRegion} States
+          Back to {stateRegion} Country
         </Link>
       </div>
     </div>
