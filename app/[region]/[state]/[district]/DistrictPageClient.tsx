16010a82a72aa42706674f1cedd9c4b77d146c50
BK Jinesh <bkjinesh@BKs-MacBook-Pro.local>
2025-05-05 18:44:11 +0530

    footer text rename

--- app/[region]/[state]/[district]/DistrictPageClient.tsx
+++ app/[region]/[state]/[district]/DistrictPageClient.tsx
@@ -202,7 +202,7 @@ export default function DistrictPageClient({
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
           </svg>
-          Back to {actualState} Districts
+          Back to {actualState} State
         </Link>
       </div>
     </div>
