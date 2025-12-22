6377bc4f22ef631e55b7ac7d78851c63864444bf
bkitwing admin <admin@bkitwing.org>
2025-12-22 13:46:51 +0530

    retreat centers

--- lib/retreatCenters.ts
+++ lib/retreatCenters.ts
@@ -5,18 +5,18 @@ export const RETREAT_CENTER_BRANCH_CODES = [
   "90006", // Center name
   "04543", // Center name
   "01758", // Center name
-  "04195", // Center name
+//  "04195", // Center name PURC
   "03793", // Center name
-  "03724", // Center name
-  "03180", // Center name
-  "02755", // Center name
-  "02417", // Center name
+//  "03724", // Center name Madhurai
+//  "03180", // Center name Thiruvananthapuram
+//  "02755", // Center name Kochi
+//  "02417", // Center name Cuttack
   "02284", // Center name
   "00858", // Center name
   "00510", // Center name
   "00386", // Center name
   "00346", // Center name
-  "00182"  // Center name
+//  "00182"  // Center name Puri
 ];
 
 // Function to check if a branch code is a retreat center
